18a0b67606bc3620299eb48773a1674cf014ed52
DooHwanKim0419 <dkimh0419@gmail.com>
2023-11-16 14:43:22 -0600

    Fix intensity

--- src/utility/sentiment.js
+++ src/utility/sentiment.js
@@ -1,25 +1,21 @@
 import vader from "vader-sentiment";
 
 const getPostiveNews = (reports) => {
-    const positiveNews = [];
-    console.log("reports in getPositiveNews\n")
-    console.log(reports);
-    reports.forEach((report) => {
-        console.log(report); 
-        const { snippet, description } = report;
-        console.log(`snippet: ${snippet}`);
-        console.log(`report: ${Object.keys(report)}`);
-        console.log(`description: ${report.description}`);
-        console.log(`Snippet: ${report.snippet}`);
-        // console.log(`report: ${Object.keys(report)}`);
-        //-1 => most negative, -1 => most positive! 
-        const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(description);
-        console.log(report, intensity);
-        if (intensity.compound > 0.8) positiveNews.push(report)
-    });
-    return positiveNews
-}
+  const positiveNews = [];
 
-export { getPostiveNews } ;
+  reports.forEach((report) => {
+    const { description } = report;
+    const intensity =
+      vader.SentimentIntensityAnalyzer.polarity_scores(description);
+
+    if (intensity.compound > 0.05) {
+      positiveNews.push(report);
+    }
+  });
+
+  return positiveNews;
+};
+
+export { getPostiveNews };
 
 export default getPostiveNews;
